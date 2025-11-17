abb0ad74fd1370af06d861059f3c6ffae26c99d2
anakafeel <hashmisaim037@gmail.com>
2025-11-17 16:00:15 -0500

    feat: add metadata image
    feat: add metadata image
    
    feat: add metadata image

--- 2025/app/layout.tsx
+++ 2025/app/layout.tsx
@@ -1,15 +1,29 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 
-const inter = Inter({
-  subsets: ['latin'],
-});
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: { default: "ARC Docs", template: "%s — ARC Docs" },
+
+  icons: {
+    icon: [
+      { url: "/images/favicon.ico" },
+      { url: "/images/favicon.png", type: "image/png" },
+    ],
+    shortcut: ["/favicon.ico"],
+    apple: ["/apple-icon.png"],
+  },
+};
+
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+  
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
